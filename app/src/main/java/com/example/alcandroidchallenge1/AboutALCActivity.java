50d909ab250157dbfea10870b90498f2c9453784
samaneng2002 <40008295+samaneng2002@users.noreply.github.com>
2019-07-13 13:17:01 +0200

    final version

--- app/src/main/java/com/example/alcandroidchallenge1/AboutALCActivity.java
+++ app/src/main/java/com/example/alcandroidchallenge1/AboutALCActivity.java
@@ -1,13 +1,55 @@
 package com.example.alcandroidchallenge1;
 
+import android.graphics.Bitmap;
+import android.net.http.SslError;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class AboutALCActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_about_alc);
+
+
+        final WebView myWebView = (WebView) findViewById(R.id.my_web_view);
+
+        myWebView.setWebViewClient(new myWebClient());
+        myWebView.measure(100, 100);
+        myWebView.getSettings().setUseWideViewPort(true);
+        myWebView.getSettings().setLoadWithOverviewMode(true);
+        myWebView.loadUrl("https://andela.com/alc/");
+
     }
+
+
+
+    public class myWebClient extends WebViewClient
+    {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            // TODO Auto-generated method stub
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            // TODO Auto-generated method stub
+            view.loadUrl(url);
+            return true;
+        }
+
+        @Override
+        public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+            handler.proceed(); // Ignore SSL certificate errors
+        }
+    }
+
+
+
 }
--- app/src/main/java/com/example/alcandroidchallenge1/MainActivity.java
+++ app/src/main/java/com/example/alcandroidchallenge1/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.alcandroidchallenge1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button aboutButton = (Button) findViewById(R.id.bt_about);
+        Button myProfile = (Button) findViewById(R.id.bt_profile);
+
+        aboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent aboutIntent = new Intent(MainActivity.this, AboutALCActivity.class);
+                startActivity(aboutIntent);
+            }
+        });
+
+        myProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myProfileIntent = new Intent(MainActivity.this, MyProfileActivity.class);
+                startActivity(myProfileIntent);
+            }
+        });
+
     }
+
+
 }
